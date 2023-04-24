b2ef3c7e728f9b97cfe12c94f43632c013567f20
NedimNN <nurkovicnedim@gmail.com>
2023-04-24 17:52:46 +0200

    Fixes to AddGoal and EditAccount pages

--- pages/AccountPages/EditAccount.js
+++ pages/AccountPages/EditAccount.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import { Modal, Portal, Provider } from 'react-native-paper'
+import DatePicker from 'react-native-modern-datepicker'
+import { getFormatedDate } from 'react-native-modern-datepicker'
+
 
 const screenWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
@@ -61,6 +65,28 @@ const styles = StyleSheet.create({
         fontSize: 16,
         height: 56,
         lineHeight: 56,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        width: '90%',
+        padding: 20,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    calendarBtns: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        columnGap: 35,
     }
 })
 
@@ -73,90 +99,151 @@ export default function EditAccount() {
     const [confirmPass, setConfirm] = useState("");
     const [hidePass, setHidePass] = useState(true);
     const [hidePassConf, setHidePassConf] = useState(true);
+    const [date, setDate] = useState(new Date);
+    const [show, setShow] = useState(false);
+    const [dateText, setDateText] = useState('Click here to pick a date');
+
+    const showModal = () => {
+        setShow(!show);
+    }
+    const showModalCancel = () => {
+        setDateText('Click here to pick a date');
+        setShow(!show);
+    }
+    const onChange = (selectedDate) => {
+        const formated = selectedDate.replaceAll('/','-');
+        const currentDate = new Date(formated);
+        setDate(currentDate);
+        setDateText(selectedDate);
+    }
 
     return (
-        <ScrollView>
-            <View style={styles.container}>
-                <View>
-                    <Text style={styles.headerText}>Edit Your Profile</Text>
-                </View>
-                <View style={styles.accountEditWrapper}>
-                    <View style={styles.accountEditInputWrapper}>
-                        <MaterialCommunityIcons name='account-outline' color={'#4A3780'} size={35} />
-                        <TextInput
-                            onChangeText={(text) => setName(text)}
-                            value={name}
-                            placeholder='e.g. John Doe'
-                            style={styles.accountInputField}
-                        />
+        <Provider>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View>
+                        <Text style={styles.headerText}>Edit Your Profile</Text>
                     </View>
-                    <View style={styles.accountEditInputWrapper}>
-                        <MaterialIcons name='date-range' color={'#4A3780'} size={35} />
-                        {/* This needs to be a date pciker */}
-                        <TextInput
-                            onChangeText={(text) => { }}
-                            value={''}
-                            placeholder='e.g. John Doe'
-                            style={styles.accountInputField}
-                        />
+                    <View style={styles.accountEditWrapper}>
+                        <View style={styles.accountEditInputWrapper}>
+                            <MaterialCommunityIcons name='account-outline' color={'#4A3780'} size={35} />
+                            <TextInput
+                                onChangeText={(text) => setName(text)}
+                                value={name}
+                                placeholder='e.g. John Doe'
+                                style={styles.accountInputField}
+                            />
+                        </View>
+                        <View style={styles.accountEditInputWrapper}>
+                            <MaterialIcons name='date-range' color={'#4A3780'} size={35} />
+                            <TouchableOpacity onPress={() => showModal()}>
+                                <Text style={styles.accountInputField}>
+                                    {dateText}
+                                </Text>
+                            </TouchableOpacity>
+                            <Portal>
+                                <Modal
+                                    animationType='slide'
+                                    transparent={true}
+                                    visible={show}
+                                >
+                                    <View >
+                                        <View style={styles.modalView}>
+                                            <DatePicker
+                                                mode='calendar'
+                                                selected={getFormatedDate(date,'DD/MM/YYYY')}
+                                                onDateChange={onChange}
+                                                options={{
+                                                    textHeaderColor: '#4A3780',
+                                                    textDefaultColor: '#4A3780',
+                                                    selectedTextColor: '#ffffff',
+                                                    mainColor: '#4A3780',
+                                                    textSecondaryColor: '#4A3780',
+                                                }}
+                                            />
+                                            <View style={styles.calendarBtns}>
+                                                <TouchableOpacity onPress={showModal}>
+                                                    <Text style={{
+                                                        color: '#4A3780',
+                                                        fontSize: 18,
+                                                        fontWeight: 'bold'
+                                                    }}>Confirm</Text>
+                                                </TouchableOpacity>
+                                                <TouchableOpacity onPress={showModalCancel}>
+                                                    <Text style={{
+                                                        color: '#4A3780',
+                                                        fontSize: 18,
+                                                        fontWeight: 'bold'
+                                                    }}>Close</Text>
+                                                </TouchableOpacity>
+                                            </View>
+
+                                        </View>
+                                    </View>
+
+                                </Modal>
+                            </Portal>
+
+                        </View>
+                        <View style={styles.accountEditInputWrapper}>
+                            <MaterialCommunityIcons name='phone' color={'#4A3780'} size={35} />
+                            <TextInput
+                                onChangeText={(text) => setPhone(text)}
+                                value={phone}
+                                placeholder='e.g. 000/000-000'
+                                style={styles.accountInputField}
+                            />
+                        </View>
+                        <View style={styles.accountEditInputWrapper}>
+                            <MaterialCommunityIcons name='email-outline' color={'#4A3780'} size={35} />
+                            <TextInput
+                                onChangeText={(text) => setEmail(text)}
+                                value={email}
+                                placeholder='e.g. john.doe@gmail.com'
+                                style={styles.accountInputField}
+                            />
+                        </View>
+                        <View style={styles.accountEditInputWrapper}>
+                            <MaterialCommunityIcons name='lock-outline' color={'#4A3780'} size={35} />
+                            <TextInput
+                                onChangeText={(text) => setPassword(text)}
+                                value={password}
+                                placeholder='Enter password'
+                                style={styles.accountInputPassField}
+                                secureTextEntry={hidePass}
+                            />
+                            <TouchableOpacity onPress={() => setHidePass(!hidePass)}>
+                                <MaterialCommunityIcons name={hidePass ? 'eye-off-outline' : 'eye-outline'} color={'#4A3780'} size={25} />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.accountEditInputWrapper}>
+                            <MaterialCommunityIcons name='lock-check-outline' color={'#4A3780'} size={35} />
+                            <TextInput
+                                onChangeText={(text) => setConfirm(text)}
+                                value={confirmPass}
+                                placeholder='Confirm password'
+                                style={styles.accountInputPassField}
+                                secureTextEntry={hidePassConf}
+                            />
+                            <TouchableOpacity onPress={() => setHidePassConf(!hidePassConf)}>
+                                <MaterialCommunityIcons name={hidePassConf ? 'eye-off-outline' : 'eye-outline'} color={'#4A3780'} size={25} />
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                    <View style={styles.accountEditInputWrapper}>
-                        <MaterialCommunityIcons name='phone' color={'#4A3780'} size={35} />
-                        <TextInput
-                            onChangeText={(text) => setPhone(text)}
-                            value={phone}
-                            placeholder='e.g. 000/000-000'
-                            style={styles.accountInputField}
-                        />
-                    </View>
-                    <View style={styles.accountEditInputWrapper}>
-                        <MaterialCommunityIcons name='email-outline' color={'#4A3780'} size={35} />
-                        <TextInput
-                            onChangeText={(text) => setEmail(text)}
-                            value={email}
-                            placeholder='e.g. john.doe@gmail.com'
-                            style={styles.accountInputField}
-                        />
-                    </View>
-                    <View style={styles.accountEditInputWrapper}>
-                        <MaterialCommunityIcons name='lock-outline' color={'#4A3780'} size={35} />
-                        <TextInput
-                            onChangeText={(text) => setPassword(text)}
-                            value={password}
-                            placeholder='Enter password'
-                            style={styles.accountInputPassField}
-                            secureTextEntry={hidePass}
-                        />
-                        <TouchableOpacity onPress={() => setHidePass(!hidePass)}>
-                            <MaterialCommunityIcons name={hidePass ? 'eye-off-outline' : 'eye-outline'} color={'#4A3780'} size={25} />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.accountEditInputWrapper}>
-                        <MaterialCommunityIcons name='lock-check-outline' color={'#4A3780'} size={35} />
-                        <TextInput
-                            onChangeText={(text) => setConfirm(text)}
-                            value={confirmPass}
-                            placeholder='Confirm password'
-                            style={styles.accountInputPassField}
-                            secureTextEntry={hidePassConf}
-                        />
-                        <TouchableOpacity onPress={() => setHidePassConf(!hidePassConf)}>
-                            <MaterialCommunityIcons name={hidePassConf ? 'eye-off-outline' : 'eye-outline'} color={'#4A3780'} size={25} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                <View style={{
-                    width: 358
-                }}>
-                    <TouchableOpacity style={styles.submitButton} onPress={() => {
-                        //TODO: Send request and update user data
-                        Alert.alert("Something went wrong", "Try again later!");
+                    <View style={{
+                        width: 358
                     }}>
-                        <Text style={styles.submitButtonText}>Save changes</Text>
-                    </TouchableOpacity>
+                        <TouchableOpacity style={styles.submitButton} onPress={() => {
+                            //TODO: Send request and update user data
+                            Alert.alert("Something went wrong", "Try again later!");
+                        }}>
+                            <Text style={styles.submitButtonText}>Save changes</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </Provider>
+
     )
 }
 
--- pages/GoalsPages/AddGoal.js
+++ pages/GoalsPages/AddGoal.js
@@ -4,6 +4,9 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { useState } from 'react';
 import DatePicker from 'react-native-modern-datepicker';
 import { getToday, getFormatedDate } from 'react-native-modern-datepicker'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import { Portal, Provider } from 'react-native-paper';
+
 
 const styles = StyleSheet.create({
     container: {
@@ -69,6 +72,32 @@ const styles = StyleSheet.create({
         fontSize: 16,
         height: 56,
         lineHeight: 56,
+    },
+    calendarBtns: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        columnGap: 35,
+    },
+    dateTextStyle: {
+        width: 235,
+        borderRadius: 15,
+        padding: 2,
+        fontSize: 18,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    wrapper: {
+        borderBottomColor: '#4A3780',
+        borderBottomWidth: 2,
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignSelf: 'stretch',
+        columnGap: 35,
+        margin: 15,
+        height: 55,
     }
 })
 
@@ -77,72 +106,110 @@ export default function AddGoal() {
 
     const startDate = getFormatedDate(today.setDate(today.getDate() + 1), 'YYYY/MM/DD')
 
-    const [open, setOpen] = useState(false);
-    const [date, setDate] = useState('12/12/2023');
+    const [show, setShow] = useState(false);
+    const [date, setDate] = useState(new Date);
+    const [dateText, setDateText] = useState('Click here to pick a date');
 
-    function handleOnPress() {
-        setOpen(!open);
+    const showModal = () => {
+        setShow(!show);
+    }
+    const showModalCancel = () => {
+        setDateText('Click here to pick a date');
+        setShow(!show);
     }
 
-    function handleChange(propDate) {
-        setDate(propDate);
+    const handleChange = (selectedDate) => {
+        const formated = selectedDate.replaceAll('/', '-');
+        const currentDate = new Date(formated);
+        setDate(currentDate);
+        setDateText(selectedDate);
     }
 
     return (
-        <SafeAreaView>
-            <ScrollView>
-                <View style={styles.container}>
+        <Provider>
+            <SafeAreaView>
+                <ScrollView>
+                    <View style={styles.container}>
 
-                    <View style={styles.inpContainer}>
-                        <Text style={styles.txt}>Task Title</Text>
-                        <TextInput
-                            style={styles.textInp}
-                        />
-                    </View>
-
-                    <TouchableOpacity onPress={handleOnPress}>
-                        <Text style={styles.titleText}>Choose date (on click)</Text>
-                    </TouchableOpacity>
-
-                    <Modal
-                        animationType='slide'
-                        transparent={true}
-                        visible={open}
-                    >
-
-                        <View style={styles.centeredView}>
-                            <View style={styles.modalView}>
-
-                                <DatePicker
-                                    mode='calendar'
-                                    minimumDate={startDate}
-                                    selected={date}
-                                    onDateChanged={handleChange}
-                                />
-
-                                <TouchableOpacity onPress={handleOnPress}>
-                                    <Text>Close</Text>
-                                </TouchableOpacity>
-
-                            </View>
+                        <View style={styles.inpContainer}>
+                            <Text style={styles.txt}>Goal Title</Text>
+                            <TextInput
+                                style={styles.textInp}
+                            />
                         </View>
-                    </Modal>
 
-                    <View style={styles.inpContainer}>
-                        <Text style={styles.txt}>Notes</Text>
-                        <TextInput
-                            multiline
-                            numberOfLines={8}
-                            style={styles.textInp}
-                        />
+                        <View style={styles.wrapper}>
+                            <MaterialIcons name='date-range' color={'#4A3780'} size={35} />
+                            <TouchableOpacity onPress={() => showModal()}>
+                                <Text style={styles.dateTextStyle}>
+                                    {dateText}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <Portal>
+                            <Modal
+                                animationType='slide'
+                                transparent={true}
+                                visible={show}
+                            >
+
+                                <View style={styles.centeredView}>
+                                    <View style={styles.modalView}>
+
+                                        <DatePicker
+                                            mode='calendar'
+                                            minimumDate={startDate}
+                                            selected={getFormatedDate(date, 'DD/MM/YYYY')}
+                                            onDateChange={handleChange}
+                                            options={{
+                                                textHeaderColor: '#4A3780',
+                                                textDefaultColor: '#4A3780',
+                                                selectedTextColor: '#ffffff',
+                                                mainColor: '#4A3780',
+                                                textSecondaryColor: '#4A3780',
+                                            }}
+                                        />
+
+                                        <View style={styles.calendarBtns}>
+                                            <TouchableOpacity onPress={showModal}>
+                                                <Text style={{
+                                                    color: '#4A3780',
+                                                    fontSize: 18,
+                                                    fontWeight: 'bold'
+                                                }}>Confirm</Text>
+                                            </TouchableOpacity>
+                                            <TouchableOpacity onPress={showModalCancel}>
+                                                <Text style={{
+                                                    color: '#4A3780',
+                                                    fontSize: 18,
+                                                    fontWeight: 'bold'
+                                                }}>Close</Text>
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                </View>
+                            </Modal>
+                        </Portal>
+
+
+                        <View style={styles.inpContainer}>
+                            <Text style={styles.txt}>Notes</Text>
+                            <TextInput
+                                multiline
+                                numberOfLines={8}
+                                style={styles.textInp}
+                            />
+                        </View>
+
+                        <TouchableOpacity style={styles.btnContainer}>
+                            <Text style={styles.txtSave}>Save</Text>
+                        </TouchableOpacity>
+
                     </View>
+                </ScrollView>
+            </SafeAreaView>
+        </Provider>
 
-                    <TouchableOpacity style={styles.btnContainer}>
-                        <Text style={styles.txtSave}>Save</Text>
-                    </TouchableOpacity>
-
-                </View>
-            </ScrollView>
-        </SafeAreaView>
     )
 }
