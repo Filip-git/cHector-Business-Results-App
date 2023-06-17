b4b6297d3cb6f54950572b12bf9b5cd7d656eb22
NedimNN <nurkovicnedim@gmail.com>
2023-06-17 16:25:58 +0200

    Fixes to KPIScreen

--- pages/KPIScreen.js
+++ pages/KPIScreen.js
@@ -3,6 +3,9 @@ import { StyleSheet, SafeAreaView, Text, View, ScrollView, Dimensions } from 're
 import { BarChart } from 'react-native-chart-kit';
 import { useFocusEffect } from '@react-navigation/native';
 import { baseGetRequest } from '../hooks/requestHelper';
+import { ActivityIndicator } from 'react-native-paper';
+import Entypo from 'react-native-vector-icons/Entypo';
+
 
 const screenWidth = Dimensions.get('window').width;
 
@@ -11,6 +14,7 @@ export default function KPIScreen() {
   const [labelSales, setLabelSales] = useState([]);
   const [dataRevenue, setDataRevenue] = useState([]);
   const [labelRevenue, setLabelRevenue] = useState([]);
+  const [animate, setAnimate] = useState(true);
 
   useFocusEffect(
     React.useCallback(() => {
@@ -34,6 +38,7 @@ export default function KPIScreen() {
       };
       fetchDataSales();
       fetchDataRevenue();
+      setAnimate(true);
 
       return () => {
         isActive = false;
@@ -44,33 +49,57 @@ export default function KPIScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView horizontal={false}>
-        <View style={styles.chartContainer}>
-          <Text style={styles.title}>Sales per year</Text>
-          <BarChart
-            style={styles.barChartStyle}
-            data={{
-              labels: labelSales,
-              datasets: [{ data: dataSales }],
-            }}
-            width={screenWidth}
-            height={215}
-            chartConfig={styles.chartConfig}
-          />
-        </View>
+        {dataSales.length === 0 && dataRevenue.length === 0 &&
+          (
+            <View>
+              {setTimeout(() => {
+                setAnimate(false);
+              }, 7000) &&
+                <View style={animate ? { ...styles.emptyWrapper, padding: 5 } : { display: 'none' }}>
+                  <ActivityIndicator size={'large'} color='#4A3780' animating={animate} style={animate ? {} : { display: 'none' }} />
+                </View>
+              }
+              {!animate && <View style={{
+                ...styles.emptyWrapper, padding: 5, marginTop: 0, marginBottom: 0
+              }}>
+                <Text style={styles.emptyText}>Sorry, no data to show</Text>
+                <Entypo name='emoji-sad' color={'#4A3780'} size={35} />
+                <Text style={styles.emptyText}>Try again later... </Text>
+              </View>}
+            </View>
+          )}
+        {dataSales.length > 0 &&
+          <View style={styles.chartContainer}>
+            <Text style={styles.title}>Sales per year</Text>
+            <BarChart
+              style={styles.barChartStyle}
+              data={{
+                labels: labelSales,
+                datasets: [{ data: dataSales }],
+              }}
+              width={screenWidth}
+              height={215}
+              chartConfig={styles.chartConfig}
+            />
+          </View>
+        }
 
-        <View style={styles.chartContainer}>
-          <Text style={styles.title}>Revenue per year </Text>
-          <BarChart
-            style={styles.barChartStyle}
-            data={{
-              labels: labelRevenue,
-              datasets: [{ data: dataRevenue }],
-            }}
-            width={screenWidth}
-            height={215}
-            chartConfig={styles.chartConfig}
-          />
-        </View>
+        {dataRevenue.length > 0 &&
+          <View style={styles.chartContainer}>
+            <Text style={styles.title}>Revenue per year </Text>
+            <BarChart
+              style={styles.barChartStyle}
+              data={{
+                labels: labelRevenue,
+                datasets: [{ data: dataRevenue }],
+              }}
+              width={screenWidth}
+              height={215}
+              chartConfig={styles.chartConfig}
+            />
+          </View>
+        }
+
       </ScrollView>
     </SafeAreaView>
   );
@@ -110,4 +139,17 @@ const styles = StyleSheet.create({
   title: {
     textAlign: 'center',
   },
+  emptyWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  emptyText: {
+    fontWeight: 'bold',
+    fontSize: 35,
+    color: '#4A3780'
+  }
 });
